48a4255f76cc9270b23674e5d3a62398b6c0012e
GEOPREDICT <geos@GEOPREDICTs-MacBook-Air.local>
2023-06-15 02:55:13 -0700

    Added workflow

---
+++ infra/main.tf
@@ -0,0 +1,131 @@
+terraform {
+  required_providers {
+    aws = {
+      version = ">= 4.0.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# specify the provider region
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_iam_user" "gh" {
+  name = "gh-action"
+}
+
+locals {
+  s3_arn = "arn:aws:s3:::terraform-20230322013953818100000001"
+}
+
+data "aws_iam_policy_document" "gh" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:*", "lambda:*"]
+    resources = ["${local.s3_arn}", "${local.s3_arn}/*", "*"]
+  }
+}
+
+resource "aws_iam_user_policy" "gh" {
+  name   = "gh-action"
+  user   = aws_iam_user.gh.name
+  policy = data.aws_iam_policy_document.gh.json
+}
+
+# the locals block is used to declare constants that 
+# you can use throughout your code
+locals {
+  function_name = "cicd"
+  handler_name  = "main.handler"
+  artifact_name = "artifact.zip"
+}
+
+# create a role for the Lambda function to assume
+# every service on AWS that wants to call other AWS services should first assume a role and
+# then any policy attached to the role will give permissions
+# to the service so it can interact with other AWS services
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+resource "aws_iam_role" "lambda" {
+  name               = "iam-for-lambda-${local.function_name}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# create a Lambda function
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function
+resource "aws_lambda_function" "lambda" {
+  role          = aws_iam_role.lambda.arn
+  function_name = local.function_name
+  handler       = local.handler_name
+  s3_bucket     = "terraform-20230322013953818100000001"
+  s3_key        = local.artifact_name
+
+  # see all available runtimes here: https://docs.aws.amazon.com/lambda/latest/dg/API_CreateFunction.html#SSS-CreateFunction-request-Runtime
+  runtime = "python3.9"
+}
+
+# create a policy for publishing logs to CloudWatch
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+resource "aws_iam_policy" "logs" {
+  name        = "lambda-logging-${local.function_name}"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# attach the above policy to the function role
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.logs.arn
+}
+
+# create a Function URL for Lambda 
+# see the docs: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lambda_function_url
+resource "aws_lambda_function_url" "url" {
+  function_name      = aws_lambda_function.lambda.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["GET"]
+    allow_headers     = ["*"]
+    expose_headers    = ["keep-alive", "date"]
+  }
+}
+
+# show the Function URL after creation
+output "lambda_url" {
+  value = aws_lambda_function_url.url.function_url
+}
